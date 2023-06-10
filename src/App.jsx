2afe7f02822863683f0334104dffc0d9603ea806
Anmol Gaud <uranmol03@gmail.com>
2023-06-10 16:03:53 +0530

    Base project ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import {Home, About, Contact, Pricing, NotFound } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route index element={Home}></Route>
+        <Route path='about' element={About}></Route>
+        <Route path='contact' element={Contact}></Route>
+        <Route path='pricing' element={Pricing}></Route>
+        <Route path='*' element={NotFound}></Route>
+      </Routes>
+    <Footer/>
+    </BrowserRouter>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'background-primary' : '#EDF3F8',
+        'background-secondary' : '#1B262F',
+        'circle-primary' : '#D1DAE2',
+        'circle-secondary' : '#1F2C38',
+        'button-primary' : "#BA4270",
+        'button-secondary' : '#DA6D97',
+        'txt-color-1' : '#36536B',
+      },
+
+      fontFamily : {
+        'DMserif' : "'DM Serif Display', serif",
+        'PublicSans' : "'Public Sans', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
